0072578caf1387c64542c551d2be0bb4bf9b5b00
Arsam Sarabi <arsamsarabi@me.com>
2020-05-01 13:33:34 +0100

    🔧 Adds AUTH0_AUDIENCE to .env.example

--- back/index.dev.ts
+++ back/index.dev.ts
@@ -1,10 +1,3 @@
-import chalk from 'chalk'
 import 'dotenv/config'
 import './src/db/connection'
 import './src/server/server'
-
-// import { populateCategories } from './src/utils/categories'
-;(() => {
-  // populateCategories()
-  console.log(chalk.bgHex('#182C61').hex('#f1f1f1')(` 🚧  Dev server starting up!  🚧 `))
-})()
